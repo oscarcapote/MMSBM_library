e62136b8240144b933e0e08bf1dc6d7fd90d1d02
Oscar Fajardo Fontiveros <oscarcapote@hotmail.es>
2022-04-18 01:50:18 +0200

    removed the encode option in pandas read

--- __init__.py
+++ __init__.py
@@ -109,7 +109,7 @@ class nodes_layer:
         self.theta = np.random.rand((N_nodes,L))
 
     def read_file(self, filename, separator="\t"):
-        self = pd.read_csv(filename,sep=separator.encode('utf-8'), engine='python')
+        self = pd.read_csv(filename,sep=separator, engine='python')
 
     def update_N(self, N_nodes):
         '''
