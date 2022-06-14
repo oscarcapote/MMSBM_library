1b70527110eb5c98ad34b0f6ed62125985062131
oscarcapote <oscarcapote@hotmail.es>
2022-06-14 15:27:40 +0200

    Fixed omega_comp_arrays

--- functions/MMSBM_functions.py
+++ functions/MMSBM_functions.py
@@ -6,7 +6,7 @@ def omega_comp_arrays(Na,Nb,p_kl,theta,eta,K,L,links_array,links_ratings):
     print("HOLA!!")
     #new_omega = np.array(omega)
     print((Na,Nb,K,L))
-    omega = np.ones((Na,Nb,K,L))
+    omega = np.zeros((Na,Nb,K,L))
     for link  in range(len(links_ratings)):
         i = links_array[link][0]
         j = links_array[link][1]
@@ -14,7 +14,7 @@ def omega_comp_arrays(Na,Nb,p_kl,theta,eta,K,L,links_array,links_ratings):
         omega[i,j,:,:] = p_kl[:,:,rating]*(np.expand_dims(theta[i,:], axis=1)@np.expand_dims(eta[j,:],axis=0))
         suma = omega[i,j,:,:].sum()
         omega[i,j,:,:] /= suma+1e-16
-    return
+    return omega
 
 # @jit(nopython=True,locals=dict(i=int64,a=int64,k=int64,link=int64,suma=double),parallel=True)
 def omega_comp_arrays_exclusive(omega,q_ka,theta,N_nodes,metas_links_arrays_nodes):
