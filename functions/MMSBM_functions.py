48888a577aa786a813a25a0eda156831397375c5
oscarcapote <oscarcapote@hotmail.es>
2022-06-27 17:20:37 +0200

    Added new import to deal with the case that the user has not installed numba

--- functions/MMSBM_functions.py
+++ functions/MMSBM_functions.py
@@ -1,5 +1,9 @@
 import numpy as np
-from numba import jit,int64,float64,double,prange,vectorize
+try:
+    from numba_functions import *
+    numba_imported = True
+except ImportError:
+    numba_imported = False
 
 # @jit(nopython=True,locals=dict(i=int64,j=int64,L=int64,K=int64,Na=int64,Nb=int64,suma=double))
 def omega_comp_arrays(Na,Nb,p_kl,theta,eta,K,L,links_array,links_ratings):
@@ -18,7 +22,7 @@ def omega_comp_arrays(Na,Nb,p_kl,theta,eta,K,L,links_array,links_ratings):
 # @jit(nopython=True,locals=dict(i=int64,a=int64,k=int64,link=int64,suma=double),parallel=True)
 def omega_comp_arrays_exclusive(q_ka,N_att,theta,N_nodes,K,metas_links_arrays_nodes):
     omega = np.zeros((N_nodes,N_att,K))
-    for j in prange(len(metas_links_arrays_nodes)):
+    for j in range(len(metas_links_arrays_nodes)):
         i = metas_links_arrays_nodes[j,0]
         a = metas_links_arrays_nodes[j,1]
         s = 0
@@ -29,62 +33,53 @@ def omega_comp_arrays_exclusive(q_ka,N_att,theta,N_nodes,K,metas_links_arrays_no
     return omega
 
 
-
-@vectorize([float64(float64,float64)])
-def A_lambda(A,lambda_val):
-    """
-    Multiplies a matrix A, by a scalar lambda_val
-    """
-    return A*lambda_val
-
+if not numba_imported:
+    def A_lambda(A,lambda_val):
+        """
+        Multiplies a matrix A, by a scalar lambda_val
+        """
+        return A*lambda_val
 
 
-@jit([float64[:,:](float64[:,:,:,:])])
-def sum_omega_02(omega):
-    """
-    Sum over axis 0 and 2 the omega matrix. Is equivalent to np.sum(omega, axis=(0,2))
-    """
-    return omega.sum(axis=0).sum(axis=1)
-    
-@jit([float64[:,:](float64[:,:,:,:])])
-def sum_omega_13(omega):
-    """
-    Sum over axis 1 and 3 the omega matrix. Is equivalent to np.sum(omega, axis=(1,3))
-    """
-    return omega.sum(axis=1).sum(axis=2)
+
+    def sum_omega_02(omega):
+        """
+        Sum over axis 0 and 2 the omega matrix. Is equivalent to np.sum(omega, axis=(0,2))
+        """
+        return omega.sum(axis=(0,2))#.sum(axis=1)
+
+    def sum_omega_13(omega):
+        """
+        Sum over axis 1 and 3 the omega matrix. Is equivalent to np.sum(omega, axis=(1,3))
+        """
+        return omega.sum(axis=(1,3))#.sum(axis=2)
 
 
-    
-@jit([float64[:,:](float64[:,:,:],int64)])
-def sum_omega_ax(omega,ax):
-    """
-    Sum over axis ax the omega matrix. 
-    """
-    return omega.sum(axis=ax)
+
+    def sum_omega_ax(omega,ax):
+        """
+        Sum over axis ax the omega matrix.
+        """
+        return omega.sum(axis=ax)
 
 
 
 
-@jit([float64[:,:](float64[:,:,:,:],float64)])
-def sum_omega_02_lambda(omega,lambda_val):
-    """
-    Sum over axis 0 and 2 the omega matrix and multiplies the result by a scalar labda_val. Is equivalent to np.sum(omega, axis=(0,2))*lambda_val
-    """
-    return A_lambda(sum_omega_02(omega),lambda_val)
-    
-@jit([float64[:,:](float64[:,:,:,:],float64)])
-def sum_omega_13_lambda(omega,lambda_val):
-    """
-    Sum over axis 1 and 3 the omega matrix and multiplies the result by a scalar labda_val. Is equivalent to np.sum(omega, axis=(1,3))*lambda_val
-    """
-    return A_lambda(sum_omega_13(omega),lambda_val)
+    def sum_omega_02_lambda(omega,lambda_val):
+        """
+        Sum over axis 0 and 2 the omega matrix and multiplies the result by a scalar labda_val. Is equivalent to np.sum(omega, axis=(0,2))*lambda_val
+        """
+        return A_lambda(sum_omega_02(omega),lambda_val)
 
-    
-@jit([float64[:,:](float64[:,:,:],int64,float64)])
-def sum_omega_ax_lambda(omega,ax,lambda_val):
-    """
-    Sum over axis ax the omega matrix and multiplies the result by a scalar labda_val. Is equivalent to np.sum(omega, axis=(ax))*lambda_val
-    """
-    return A_lambda(sum_omega_ax(omega,ax),lambda_val)
+    def sum_omega_13_lambda(omega,lambda_val):
+        """
+        Sum over axis 1 and 3 the omega matrix and multiplies the result by a scalar labda_val. Is equivalent to np.sum(omega, axis=(1,3))*lambda_val
+        """
+        return A_lambda(sum_omega_13(omega),lambda_val)
 
 
+    def sum_omega_ax_lambda(omega,ax,lambda_val):
+        """
+        Sum over axis ax the omega matrix and multiplies the result by a scalar labda_val. Is equivalent to np.sum(omega, axis=(ax))*lambda_val
+        """
+        return A_lambda(sum_omega_ax(omega,ax),lambda_val)
