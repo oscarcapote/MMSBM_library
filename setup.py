cffb818a9e7c3fb2811ebefff894cca77f16e957
Oscar Fajardo Fontiveros <oscarcapote@hotmail.es>
2025-06-10 11:17:45 +0200

    Changed requirements and setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="BiMMSBM",
+    name="MMSBM_library",
     version="0.1.0",
-    packages=find_packages(include=['BiMMSBM', 'BiMMSBM.*']),
-    package_dir={'BiMMSBM': '.'},
+    packages=find_packages(include=['MMSBM_library', 'MMSBM_library.*']),
+    package_dir={'MMSBM_library': '.'},
     install_requires=[
         "numpy",
         "numba",
@@ -13,8 +13,8 @@ setup(
         "matplotlib",
     ],
     author="Oscar Fajardo Fontiveros",
-    author_email="your.email@example.com",  # Replace with your email
-    description="A library for Mixed Membership Stochastic Block Models",
+    author_email="oscarcapote@hotmail.es",  # Replace with your email
+    description="A library for Mixed Membership Stochastic Block Models in bipartite networks",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
